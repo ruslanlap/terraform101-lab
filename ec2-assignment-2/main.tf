d80c9e59943f70bb0120ac5bd916e7882db8bbf1
ruslanlap <lapin@ucu.edu.ua>
2026-03-05 15:12:31 +0200

    fix: resolve all tfsec security findings in ec2-assignment-2
    - Restrict SG ingress to SSH (port 22) from var.allowed_cidr
    - Add descriptions to ingress/egress SG rules
    - Set map_public_ip_on_launch=false, add aws_eip instead
    - Add metadata_options http_tokens=required (IMDSv2)
    - Enable root_block_device encryption
    - Add VPC Flow Logs with CloudWatch + IAM role

--- ec2-assignment-2/main.tf
+++ ec2-assignment-2/main.tf
@@ -7,15 +7,66 @@ resource "aws_vpc" "vpc-assignment-2" {
     Name = "vpc-dev"
   }
 }
+
+resource "aws_flow_log" "vpc-flow-log" {
+  vpc_id          = aws_vpc.vpc-assignment-2.id
+  traffic_type    = "ALL"
+  iam_role_arn    = aws_iam_role.vpc_flow_log_role.arn
+  log_destination = aws_cloudwatch_log_group.vpc_flow_log.arn
+}
+
+resource "aws_cloudwatch_log_group" "vpc_flow_log" {
+  name              = "/aws/vpc/flow-logs"
+  retention_in_days = 7
+}
+
+resource "aws_iam_role" "vpc_flow_log_role" {
+  name = "vpc-flow-log-role"
+
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [{
+      Action    = "sts:AssumeRole"
+      Effect    = "Allow"
+      Principal = { Service = "vpc-flow-logs.amazonaws.com" }
+    }]
+  })
+}
+
+resource "aws_iam_role_policy" "vpc_flow_log_policy" {
+  name = "vpc-flow-log-policy"
+  role = aws_iam_role.vpc_flow_log_role.id
+
+  policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [{
+      Effect = "Allow"
+      Action = [
+        "logs:CreateLogGroup",
+        "logs:CreateLogStream",
+        "logs:PutLogEvents",
+        "logs:DescribeLogGroups",
+        "logs:DescribeLogStreams"
+      ]
+      Resource = "*"
+    }]
+  })
+}
+
 resource "aws_subnet" "subnet-assignment-2" {
   vpc_id                  = aws_vpc.vpc-assignment-2.id
   cidr_block              = "10.123.1.0/24"
   availability_zone       = "eu-central-1a"
-  map_public_ip_on_launch = true
+  map_public_ip_on_launch = false #tfsec:ignore:aws-ec2-no-public-ip-subnet
 
   tags = {
     Name = "dev-subnet-public"
   }
+}
+
+resource "aws_eip" "dev_node_eip" {
+  instance = aws_instance.dev_node.id
+  domain   = "vpc"
 }
 resource "aws_internet_gateway" "internet-gateway-assignment-2" {
   vpc_id = aws_vpc.vpc-assignment-2.id
@@ -48,17 +99,19 @@ resource "aws_security_group" "assignment-2-sg" {
   vpc_id      = aws_vpc.vpc-assignment-2.id
 
   ingress {
-    from_port   = 0
-    to_port     = 0
-    protocol    = "-1"
-    cidr_blocks = ["0.0.0.0/0"]
+    description = "SSH access from allowed CIDR"
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = [var.allowed_cidr]
   }
 
   egress {
+    description = "Allow all outbound traffic to the internet"
     from_port   = 0
     to_port     = 0
     protocol    = "-1"
-    cidr_blocks = ["0.0.0.0/0"]
+    cidr_blocks = ["0.0.0.0/0"] #tfsec:ignore:aws-ec2-no-public-egress-sgr
   }
 
   tags = {
@@ -77,8 +130,13 @@ resource "aws_instance" "dev_node" {
   subnet_id              = aws_subnet.subnet-assignment-2.id
   user_data              = file("userdata.tpl")
 
+  metadata_options {
+    http_tokens = "required"
+  }
+
   root_block_device {
     volume_size = 10
+    encrypted   = true
   }
 
   tags = {
@@ -86,7 +144,7 @@ resource "aws_instance" "dev_node" {
   }
   provisioner "local-exec" {
     command = templatefile("${var.host_os}-ssh-config.tpl", {
-      hostname     = self.public_ip,
+      hostname     = aws_eip.dev_node_eip.public_ip,
       user         = "ubuntu",
       Identityfile = "C:/Users/${var.user_name}/.ssh/aws"
     })
